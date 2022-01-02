edee3c26b731caeefa8c24ed2bd8ded2a4f05dec
yosefgoren <yosefisraelgoren@gmail.com>
2022-01-02 14:51:06 +0200

    first commit

---
+++ extension.js
@@ -0,0 +1,32 @@
+const { stringify } = require('querystring');
+const vscode = require('vscode');
+
+let pending_selections = [];
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	console.log('Octopus cursors has been activated.');
+
+	let disposable1 = vscode.commands.registerCommand('hellovsc.captureCursor', function () {
+		let active_editor = vscode.window.activeTextEditor
+		const anchor_pos = active_editor.selection.anchor
+		const active_pos = active_editor.selection.active
+		vscode.window.showInformationMessage('Captured Cursor!');
+		pending_selections.push(new vscode.Selection(anchor_pos, active_pos))
+	});
+	let disposable2 = vscode.commands.registerCommand('hellovsc.actiavteCursors', function () {
+		pending_selections.unshift(vscode.window.activeTextEditor.selection) 
+		vscode.window.activeTextEditor.selections = pending_selections
+		pending_selections = []
+	});
+	context.subscriptions.push(disposable1);
+	context.subscriptions.push(disposable2);
+}
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
