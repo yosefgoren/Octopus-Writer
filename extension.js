2575a756e7aa96807dad6764c3a5520cd4e54fed
yosefgoren <yosefisraelgoren@gmail.com>
2022-01-02 22:23:22 +0200

    fixed command not found issue

--- extension.js
+++ extension.js
@@ -9,14 +9,14 @@ let pending_selections = [];
 function activate(context) {
 	console.log('Octopus cursors has been activated.');
 
-	let disposable1 = vscode.commands.registerCommand('hellovsc.captureCursor', function () {
+	let disposable1 = vscode.commands.registerCommand('octopus_writer.captureCursor', function () {
 		let active_editor = vscode.window.activeTextEditor
 		const anchor_pos = active_editor.selection.anchor
 		const active_pos = active_editor.selection.active
 		vscode.window.showInformationMessage('Captured Cursor!');
 		pending_selections.push(new vscode.Selection(anchor_pos, active_pos))
 	});
-	let disposable2 = vscode.commands.registerCommand('hellovsc.actiavteCursors', function () {
+	let disposable2 = vscode.commands.registerCommand('octopus_writer.actiavteCursors', function () {
 		pending_selections.unshift(vscode.window.activeTextEditor.selection) 
 		vscode.window.activeTextEditor.selections = pending_selections
 		pending_selections = []
